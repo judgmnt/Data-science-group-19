59e4d0faba410344996c40bc7ab4014c486c49cb
SpinazieSin <drone.grt@gmail.com>
2019-10-10 20:00:21 +0200

    Finally added the df

--- assignment2/main.py
+++ assignment2/main.py
@@ -1,15 +1,14 @@
 import numpy as np
 import pandas as pd
 import time
+import operator
 from pprint import pprint
 
 features = ["user_id","image_id","data_memorability","user_website","user_bio","user_followed_by","user_follows","user_posted_photos","face_gender","face_gender_confidence","face_age_range_high","face_age_range_low","face_sunglasses","face_beard","face_beard_confidence","face_mustache","face_mustache_confidence","face_smile","face_smile_confidence","eyeglasses","eyeglasses_confidence","face_emo","emo_confidence","comment_count","like_count","data_amz_label","data_amz_label_confidence"]
+survey_df = pd.read_pickle(r'data/survey.pickle')
+users = set(survey_df["insta_user_id"].apply(str))
+
 # anp_df = pd.read_pickle(r'data/anp.pickle')
-face_df = pd.read_pickle(r'data/face.pickle')
-image_df = pd.read_pickle(r'data/image_data.pickle')
-metrics_df = pd.read_pickle(r'data/image_metrics.pickle')
-object_labels_df = pd.read_pickle(r'data/object_labels.pickle')
-# survey_df = pd.read_pickle(r'data/survey.pickle')
 
 # survey_df = survey_df.astype({"insta_user_id": object})
 
@@ -18,74 +17,189 @@ object_labels_df = pd.read_pickle(r'data/object_labels.pickle')
 # df = pd.merge(df, object_labels_df, how='inner', on='image_id')
 # df = pd.merge(df, anp_df)
 
-users = set(image_df["user_id"])
-data = {}
-for id in users:
-    if id not in data:
-        data[id] = np.zeros(5)
+def get_user_image_data(return_user_id = True):
+    image_df = pd.read_pickle(r'data/image_data.pickle')
+    user_data = {}
+    for index, row in image_df.iterrows():
+        user_id = row["user_id"]
+        if user_id not in users:
+            continue
 
-emo_set = set(face_df["face_emo"])
-index = 0
-emo_list = {}
-for emo in emo_set:
-    emo_list[emo] = index
-    index += 1
+        if user_id not in user_data:
+            user_data[user_id] = {}
 
-face_data = {}
-for index, row in face_df.iterrows():
+        image_id = row["image_id"]
+        if image_id not in user_data[user_id]:
+            user_data[user_id][image_id] = np.zeros(4)
 
-    image_id = row["image_id"]
-    if image_id not in face_data:
-        face_data[image_id] = np.zeros(16)
+        user_data[user_id][image_id][0] += 1 if row["image_filter"] == "Normal" else 0
 
-    # 1-2 female male
-    if row["face_gender"] == "Female":
-        face_data[image_id][0] += 1
-    else:
-        face_data[image_id][1] += 1
+        user_data[user_id][image_id][1] = 1 if row["user_website"] else 0
 
-    # 3 - face age
-    face_data[image_id][2] += (row["face_age_range_low"]+row["face_age_range_high"])/2
+        user_data[user_id][image_id][2] = row["user_followed_by"]
 
-    # 4 - sunglasses
-    face_data[image_id][3] += 1 if row["face_sunglasses"] else 0
+        user_data[user_id][image_id][3] = row["user_posted_photos"]
 
-    # 5 - beard
-    face_data[image_id][4] += 1 if row["face_beard"] and row["face_beard_confidence"] > 90. else 0
+    columns = ["Filters", "Website", "Followers", "Follows"]
+    return [user_data, columns]
 
-    # 6 - mustache
-    face_data[image_id][5] += 1 if row["face_mustache"] and row["face_mustache_confidence"] > 90. else 0
+def get_face_data():
+    face_df = pd.read_pickle(r'data/face.pickle')
 
-    # 7 - smile
-    face_data[image_id][6] += 1 if row["face_smile"] and row["face_smile_confidence"] > 90. else 0
+    emo_set = set(face_df["face_emo"])
+    index = 0
+    emo_list = {}
 
-    # 8 - eyeglasses
-    face_data[image_id][7] += 1 if row["eyeglasses"] and row["eyeglasses_confidence"] > 90. else 0
+    for emo in emo_set:
+        emo_list[emo] = index
+        index += 1
 
-    # 9 - 15
-    if row["emo_confidence"] > 60.:
-        face_data[image_id][emo_list[row["face_emo"]]] += 1
-    break
+    face_data = {}
+    for index, row in face_df.iterrows():
+        # if row["user_id"] not in users:
+        #     continue
 
-for id in face_data:
-    face_data[id][2] = face_data[id][2]/(face_data[id][0]+face_data[id][1])
+        image_id = row["image_id"]
+        if image_id not in face_data:
+            face_data[image_id] = np.zeros(15)
 
-metric_data = {}
-for index, row in metrics_df.iterrows():
-    image_id = row["image_id"]
-    if image_id not in metric_data:
-        metric_data[image_id] = np.zeros(2)
+        # 1-2 female male
+        if row["face_gender"] == "Female":
+            face_data[image_id][0] += 1
+        else:
+            face_data[image_id][1] += 1
+
+        # 3 - face age
+        face_data[image_id][2] += (row["face_age_range_low"]+row["face_age_range_high"])/2
+
+        # 4 - sunglasses
+        face_data[image_id][3] += 1 if row["face_sunglasses"] else 0
+
+        # 5 - beard
+        face_data[image_id][4] += 1 if row["face_beard"] and row["face_beard_confidence"] > 90. else 0
+
+        # 6 - mustache
+        face_data[image_id][5] += 1 if row["face_mustache"] and row["face_mustache_confidence"] > 90. else 0
+
+        # 7 - smile
+        face_data[image_id][6] += 1 if row["face_smile"] and row["face_smile_confidence"] > 90. else 0
+
+        # 8 - eyeglasses
+        face_data[image_id][7] += 1 if row["eyeglasses"] and row["eyeglasses_confidence"] > 90. else 0
+
+        # 9 - 15
+        if row["emo_confidence"] > 60.:
+            face_data[image_id][8+emo_list[row["face_emo"]]] += 1
+
+    # get average face age
+    for id in face_data:
+        face_data[id][2] = face_data[id][2]/(face_data[id][0]+face_data[id][1])
+
+    # Create columns
+    columns = ["Female", "Male", "Age", "Sunglasses", "Beard", "Mustache", "Smile", "Eyeglasses"]
+    for emo in sorted(emo_list.items(), key=operator.itemgetter(1)):
+        columns.append(emo[0])
+
+    return [face_data, columns]
+
+def get_metric_data():
+    metrics_df = pd.read_pickle(r'data/image_metrics.pickle')
+    metric_data = {}
+    for index, row in metrics_df.iterrows():
+        # if row["user_id"] not in users:
+        #     continue
+
+        image_id = row["image_id"]
+        if image_id not in metric_data:
+            metric_data[image_id] = np.zeros(2)
+        
+        metric_data[image_id][0] += row["like_count"]
+        metric_data[image_id][1] += row["comment_count"]
+
+    columns = ["Likes", "Comments"]
+    return [metric_data, columns]
+
+def get_object_count():
+    object_labels_df = pd.read_pickle(r'data/object_labels.pickle')
+    object_count = {}
+    for index, row in object_labels_df.iterrows():
+        # if row["user_id"] not in users:
+        #     continue
+
+        image_id = row["image_id"]
+        if image_id not in object_count:
+            object_count[image_id] = 0
+        
+        object_count[image_id] += 1
+    columns = ["Objects"]
+    return [object_count, columns]
+
+def main():
+    survey_df = pd.read_pickle(r'data/survey.pickle')
+    survey_df = survey_df.astype({"insta_user_id": str})
+    df = pd.read_pickle("image_df.pickle")
+    result_df = pd.read_pickle("result_df.pickle")
+    print(result_df)
+
+    #################### CREATE FINAL DF
+    # [total_data, total_cols] = get_user_image_data()
+    # df = df.fillna(0)
+
+    # index = 0
+    # result = {}
+    # for user in total_data:
+    #     print("User: ", user)
+    #     total_rows = []
+    #     for image_id in total_data[user]:
+    #         row = list(df[df.index == image_id].values.flatten())
+    #         if len(row) < 1: continue
+    #         for element in total_data[user][image_id]:
+    #             row.append(element)
+    #         total_rows.append(row)
+    #     if not row: continue
+    #     result[user] = np.average(total_rows, axis=0)
+        
+    #     print(index)
+    #     index += 1
+
+    # columns = list(df.columns)
+    # for col in total_cols:
+    #     columns.append(col)
+    # result_df = pd.DataFrame.from_dict(result, orient='index')
+    # result_df.columns = columns
+    # result_df.to_pickle("result_df.pickle")
+    # print(result_df[:10])
+
+    ################ CREATE DICTS
+
+    # [metric_data, metric_cols] = get_metric_data()
+    # df1 = pd.DataFrame.from_dict(metric_data, orient='index')
+    # df1.columns = metric_cols
+    # print("1", len(df1))
     
-    metric_data[image_id][0] += row["like_count"]
-    metric_data[image_id][1] += row["comment_count"]
+    # [face_data, face_cols] = get_face_data()
+    # df2 = pd.DataFrame.from_dict(face_data, orient='index')
+    # df2.columns = face_cols
+    # print("2", len(df2))
 
-object_count = {}
-for index, row in object_labels_df.iterrows():
-    image_id = row["image_id"]
-    if image_id not in object_count:
-        object_count[image_id] = 0
-    
-    object_count[image_id] += 1
+    # [object_data, object_cols] = get_object_count()
+    # df3 = pd.DataFrame.from_dict(object_data, orient='index')
+    # df3.columns = object_cols
+    # print("3", len(df3))
+
+    # df = df1.merge(df2, how='outer', left_index=True, right_index=True)
+    # df = df.merge(df3, how='outer', left_index=True, right_index=True)
+    # print(df.shape)
+    # print(len(df))
+    # print(df.columns)
+
+    # df.to_pickle("image_df.pickle")
+
+    index_list = df.index
+    print()
+
+if __name__ == '__main__':
+    main()
 
 # # anp_df 325941 ['image_id', 'anp_label', 'anp_sentiment', 'emotion_score', 'emotion_label']
 # print("\nanp_df", len(anp_df), list(anp_df.columns))
